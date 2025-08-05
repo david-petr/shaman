16ed38c06c324f060deaa6c625d436a2e2d77743
David Petr <petrhk.david@gmail.com>
2025-08-05 10:06:45 +0200

    Uprava aktualizační lišty - nevyzkoušeno

--- src/js/index.js
+++ src/js/index.js
@@ -15,13 +15,36 @@ window.accentColor.onUpdated((color) => {
   document.documentElement.style.setProperty("--update-back", Color.makeHexOpaque(Color.darken(color, 40)))
 })
 
+localStorage.setItem("update", JSON.stringify(true))
+
 // ==== app update info ====
 document.addEventListener("DOMContentLoaded", () => {
+  console.log(JSON.parse(localStorage.getItem("update")))
+  if(JSON.parse(localStorage.getItem("update"))){
+    window.updateProgress.get( progress => {
+      console.log(progress)
+      if(progress){
+        window.windowApi.resizeWindow(300, 460)
+        updateSection.style.display = "flex"
+        updateMessageElement.style.display = "none"
+        progressBar.style.display = "flex"
+        window.updateProgress.get( progress => {
+          progressElement.value = progress.percent
+        })
+      } else {
+        progressBar.style.display = "none"
+        updateMessageElement.style.display = "block"
+        updateMessageElement.textContent = "Pro dokončení aktualizace zavřete aplikaci"
+      }
+    })
+  }
+
   window.sendMessage.get( message => {
     if(message === "available"){
       window.windowApi.resizeWindow(300, 460)
       updateMessageElement.textContent = "Nová verze aplikace je k dispozici. Stahuje se na pozadí."
       updateSection.style.display = "flex"
+      localStorage.setItem("update", JSON.stringify(true))
 
       setTimeout(() => {
         updateMessageElement.style.display = "none"
@@ -35,14 +58,17 @@ document.addEventListener("DOMContentLoaded", () => {
       progressBar.style.display = "none"
       updateMessageElement.style.display = "block"
       updateMessageElement.textContent = "Aktualizace je stažena"
+      localStorage.setItem("update", JSON.stringify(false))
 
       setTimeout(() => {
-        window.windowApi.resizeWindow(300, 425)
-        updateSection.style.display = "none"
+        // window.windowApi.resizeWindow(300, 425)
+        // updateSection.style.display = "none"
+        updateMessageElement.textContent = "Pro dokončení aktualizace zavřete aplikaci"
       }, 3000)
 
     } else {
       console.log(message)
+      localStorage.setItem("update", JSON.stringify(false))
     }
   })
 })
