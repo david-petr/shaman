4ca25e0123cc505450151f9c31610d8c4e495972
David Petr <petrhk.david@gmail.com>
2025-08-07 10:40:06 +0200

    Příprava vlajek část 1.

--- src/js/flags/flags.js
+++ src/js/flags/flags.js
@@ -1,12 +1,19 @@
 // ==== Globální proměnné ====
-let allMapData = null
+let allData = null
 const preferences = JSON.parse(localStorage.getItem("preferences"))
 const continent = preferences.continent
 
 const initializeGame = async () => {
-    allMapData = await DataImport.getData("../../data/map.json")
+    allData = await DataImport.getData("../../data/map.json")
 
-    
+    const countries = [...allData[continent].countries]
+
+    countries.forEach( country => {
+        const img = document.createElement("img")
+        img.setAttribute("src", "../../img/flags/" + country.id + ".webp")
+
+        document.querySelector("body").appendChild(img)
+    })
 }
 
 initializeGame()
