df8cd55dbbf88ffc88662c64a84d58824c35916c
David Petr <petrhk.david@gmail.com>
2025-09-01 19:37:02 +0200

    Posouvání a přibližování / oddalování mapy

--- src/js/country-finding/main.js
+++ src/js/country-finding/main.js
@@ -1,3 +1,103 @@
+// ==== Globální proměnné ====
+let data
+
+
+// ==== functions ====
+const initializeGame = async () => {
+    data = await DataImport.getData("../../data/country-finding.json")
+
+    console.log(data)
+
+
+    // ==== Nastavení viewBox pro mapu ====
+    let focusCountry = data.countries.filter(country => {
+        return country.id === "cz"
+    })
+    focusCountry = focusCountry[0]
+
+    const worldMap = document.getElementById("map")
+    worldMap.setAttribute('viewBox', `${focusCountry.position.x} ${focusCountry.position.y} 2500 1000`)
+    document.documentElement.style.setProperty("--scale", (Math.floor(focusCountry.position.scale / 5)))
+
+    // ==== eventy ====
+    let isDragging = false
+    let startX, startY
+    let viewBoxX, viewBoxY
+    let scale
+    let viewBox
+
+    worldMap.addEventListener("mousedown", (e) => {
+        isDragging = true
+        worldMap.style.cursor = "grabbing"
+
+        startX = e.clientX
+        startY = e.clientY
+
+        viewBox = worldMap.getAttribute("viewBox").split(" ").map(Number)
+        viewBoxX = viewBox[0]
+        viewBoxY = viewBox[1]
+    })
+    worldMap.addEventListener("mousemove", (e) => {
+        if (!isDragging) return
+
+        const deltaX = e.clientX - startX
+        const deltaY = e.clientY - startY
+
+        
+        const cssScale = parseFloat(document.documentElement.style.getPropertyValue("--scale"))
+        scale = (viewBox[2] / worldMap.clientWidth) * (1 / cssScale)
+
+        const newViewBoxX = viewBoxX - (deltaX * scale)
+        const newViewBoxY = viewBoxY - (deltaY * scale)
+
+        worldMap.setAttribute("viewBox", `${newViewBoxX} ${newViewBoxY} 2500 1000`)
+    })
+    worldMap.addEventListener("mouseup", () => {
+        isDragging = false
+        worldMap.style.cursor = "move"
+    })
+    worldMap.addEventListener("mouseleave", () => {
+        isDragging = false
+        worldMap.style.cursor = "move"
+    })
+    worldMap.addEventListener("wheel", (e) => {
+        e.preventDefault()
+
+        let scale = parseFloat(document.documentElement.style.getPropertyValue("--scale"))
+        
+        if(e.deltaY < 0){
+            scale += 0.5
+        } else {
+            scale -= 0.5
+        }
+
+        if(scale >= 1){
+            document.documentElement.style.setProperty("--scale", scale)
+        }
+    })
+
+    // ==== odstranění prvků ====
+    document.querySelector("#ocean")?.remove()
+    const titles = document.querySelector("body").querySelectorAll("title")
+    titles.forEach((title) => {
+        title.remove()
+    })
+}
+
+document.addEventListener("DOMContentLoaded", initializeGame)
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ==== accent color => css variables ====
 window.accentColor.get().then(color => {
     document.documentElement.style.setProperty("--accent-color", Color.makeHexOpaque(color))
